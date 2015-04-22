ae08ff8315a6ebc144a28172f60f98e2052ea8cf
Andrew Barbour <andrew.barbour1@gmail.com>
2015-04-22 10:00:26 +1000

    Created base Ai movement script (should be used for all navagents), and created new folder structure.

---
+++ McRaid/Assets/Scripts/MovementBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBehaviour : MonoBehaviour 
+{
+	protected NavMeshAgent _agent;
+	protected Vector3 _goal;
+	
+	void Start () 
+	{
+		_agent 	= GetComponent<NavMeshAgent>();
+	}
+
+	void Update () 
+	{
+
+	}
+
+	protected void FindPath(Vector3 a_goal)
+	{
+		_agent.destination 	=	a_goal;
+		_goal 				= a_goal;
+	}
+}
---
+++ McRaid/Assets/Scripts/PlayerAIBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAIBehaviour : MovementBehaviour 
+{
+	void Start () 
+	{
+		FindPath(_goal);
+	}
+
+	void Update () 
+	{
+		
+	}
+}
