2264f3a763dce20db0cdedafecaf108a15efc259
Andrew Barbour <andrew.barbour1@gmail.com>
2015-04-29 15:47:09 +1000

    Cleaned scene hierarchy, and added spawning system.

---
+++ McRaid/Assets/Scripts/AIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIManager : MonoBehaviour 
+{
+    List<BearSoldier> _soliderList;
+
+	void Start() 
+    {
+        
+	}
+	
+	void Update() 
+    {
+	    
+	}
+}
---
+++ McRaid/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPoint : MonoBehaviour 
+{
+    public float _health;
+    public float _speed;
+    public float _sightRange;
+    public float _weaponReloadTime;
+    public float _weaponRange;
+    public float _weaponFireRate;
+    public float _weaponDamage;
+    public float _weaponAmmo;
+
+    public GameObject _AIPrefab;
+
+	void Start() 
+    {
+        // Create object
+        GameObject gameObject = Instantiate(_AIPrefab);
+        gameObject.transform.position = transform.position;
+
+        // Initialise stats
+        BearSoldier solider = gameObject.GetComponent<BearSoldier>();
+        solider._maxHealth          = _health;
+        solider._currentHealth      = _health;
+        solider._speed              = _speed;
+        solider._sightRange         = _sightRange;
+        solider._weaponReloadTime   = _weaponReloadTime;
+        solider._weaponRange        = _weaponRange;
+        solider._weaponFireRate     = _weaponFireRate;
+        solider._weaponDamage       = _weaponDamage;
+        solider._weaponAmmo         = _weaponAmmo;
+	}
+}
